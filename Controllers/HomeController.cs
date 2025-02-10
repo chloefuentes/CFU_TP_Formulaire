b8f08999290bbd5f8575c65cc142163b0cffffdd
chloefuentes <chloe.fuentes05@gmail.com>
2025-02-10 15:05:38 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TP2.Models;
+using TPLOCAL;
+
+namespace TP2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Avis()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "resources", "DataAvis.xml");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("Fichier introuvable.");
+            }
+
+            ListReviews listReviews = new ListReviews();
+            List<Reviews> avisList = listReviews.GetAvis(filePath);
+
+            return View("Avis", avisList);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static List<Validation> _validations = new List<Validation>();
+
+        [HttpPost]
+        public IActionResult SubmitForm(Validation model)
+        {
+            if (ModelState.IsValid)
+            {
+                _validations.Add(model);
+                return RedirectToAction("PageDeValidation");
+            }
+            return View("Form", model);
+        }
+
+        public IActionResult PageDeValidation()
+        {
+            return View(_validations);
+        }
+    }
+}
---
+++ Models/ListReview.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace TPLOCAL
+{
+    public class ListReviews
+    {
+        public List<Reviews> GetAvis(string file)
+        {
+            List<Reviews> listReviews = new List<Reviews>();
+            XmlDocument xmlDoc = new XmlDocument();
+
+            
+            xmlDoc.Load(file);
+
+            foreach (XmlNode node in xmlDoc.SelectNodes("root/row"))
+            {
+                string nom = node["Nom"].InnerText;
+                string prenom = node["Prenom"].InnerText;
+                string avis = node["Avis"].InnerText;
+
+                Reviews review = new Reviews
+                {
+                    Nom = nom,
+                    Prenom = prenom,
+                    AvisDonne = avis
+                };
+
+                listReviews.Add(review);
+            }
+            return listReviews;
+        }
+    }
+
+    public class Reviews
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string AvisDonne { get; set; }
+    }
+}
